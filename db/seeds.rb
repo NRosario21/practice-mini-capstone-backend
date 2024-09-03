3c2e91f4c4847acd21d45c3d7d7aad535cb76397
NRosario21 <nosrea@gmail.com>
2024-09-03 10:42:27 -0500

    Add model with seeds for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Movie.create(name: "Matrix", category: "Cyberpunk", year: 1999)
+Movie.create(name: "Legend", category: "Fantasy", year: 1986)
