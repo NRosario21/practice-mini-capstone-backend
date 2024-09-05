f6a249dc417ff1eb2bfb50b4d4fe19d143bf157c
NRosario21 <nosrea@gmail.com>
2024-09-05 11:36:00 -0500

    Add create action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,13 @@ class MoviesController < ApplicationController
     @movies = Movie.all
     render :index
   end
+
+  def create
+    @movie = Movie.create(
+      name: params[:name],
+      category: params[:category],
+      year: params[:year],
+    )
+    render :show
+  end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -8,4 +8,11 @@ class MoviesControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Movie.count, data.length
   end
+
+  test "create" do
+    assert_difference "Movie.count", 1 do
+      post "/movies.json", params: { name: "lake", width: 800, height: 600 }
+      assert_response 200
+    end
+  end
 end
